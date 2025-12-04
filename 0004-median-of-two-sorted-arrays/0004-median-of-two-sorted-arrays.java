508b6d53d12ae0f64ce9725d3d439eec3fe8e495
Nanthitha P <nanthithaponnusamy@gmail.com>
2025-12-04 11:57:09 +0530

    Time: 1 ms (99.93%), Space: 48.9 MB (12.99%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,21 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int[] merge = new int[n1+n2];
-
-        System.arraycopy(nums1,0,merge,0,n1);
-        System.arraycopy(nums2,0,merge,n1,n2);
-        
-        Arrays.sort(merge);
-        int len = merge.length;
-
-        if(len%2==0){
-            return (merge[len/2 -1] + merge[len/2])/2.0;
+        if(nums1.length >nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
         }
-        else{
-            return merge[len/2];
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int low=0;
+        int high=nums1.length;
+        while(low<=high){
+            int cut1=(low+high)/2;
+            int cut2=(n1+n2+1)/2-cut1;
+            int leftA=(cut1==0)?Integer.MIN_VALUE:nums1[cut1-1];
+            int rightA=(cut1==n1)?Integer.MAX_VALUE:nums1[cut1];
+            int leftB=(cut2==0)?Integer.MIN_VALUE:nums2[cut2-1];
+            int rightB=(cut2==n2)?Integer.MAX_VALUE:nums2[cut2];
+
+            if(leftA <=rightB && leftB <= rightA){
+                if((n1+n2)%2 == 0){
+                    return (Math.max(leftA,leftB) + Math.min(rightA ,rightB))/2.0;
+                }
+                else{
+                    return (double) Math.max(leftA,leftB);
+                }
+            }
+            else if(leftA > rightB){
+                high = cut1-1;
+            }
+            else{
+                low = cut1+1;
+            }
         }
-   
+        return 0.0;
     }
 }
