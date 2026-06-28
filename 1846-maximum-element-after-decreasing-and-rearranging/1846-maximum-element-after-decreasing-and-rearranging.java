67f8a9e369f955265b23cd2d701915187f043f59
Nanthitha P <nanthithaponnusamy@gmail.com>
2026-06-28 08:32:40 +0530

    Time: 657 ms (6.67%), Space: 74.9 MB (100%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,30 +1,30 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        if(arr[0]!=1){
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==1){
-                int temp = arr[0];
-                arr[0] = arr[i];
-                arr[i] = temp;
-            }
-           }
-        }
-        arr[0] = 1;
+        // if(arr[0]!=1){
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]==1){
+        //         int temp = arr[0];
+        //         arr[0] = arr[i];
+        //         arr[i] = temp;
+        //     }
+        //    }
+        // }
         Arrays.sort(arr);
+        arr[0] = 1;
         for(int i=0;i<arr.length-1;i++){
             if((arr[i+1] - arr[i])>1){
                arr[i+1] = arr[i]+1;
             }
         }
+        // int max = arr[0];
+        // for(int i=0;i<arr.length;i++){
+        //    if(arr[i]>max){
+        //       max = arr[i];
+        //    }
+        // }
         for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         }
-        int max = arr[0];
-        for(int i=0;i<arr.length;i++){
-           if(arr[i]>max){
-              max = arr[i];
-           }
-        }
-        return max;
+        return arr[arr.length-1];
     }
 }
