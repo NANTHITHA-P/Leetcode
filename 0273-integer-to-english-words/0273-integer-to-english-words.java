1533ef20f004d4a928d2021e4f81008f213be87a
Nanthitha P <nanthithaponnusamy@gmail.com>
2026-08-05 22:57:24 +0530

    Time: 6 ms (63.93%), Space: 46 MB (22.61%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -11,32 +11,27 @@ class Solution {
         "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
     };
     public String helper(int num){
-        if(num < 20) return below20[num]+" ";
-        if (num < 100) {
-        if (num % 10 == 0)
-            return tens[num / 10] + " ";
+        if (num < 20)
+            return below20[num];
 
-        return tens[num / 10] + " " + helper(num % 10);
-    }
-        if (num < 1000) {
-    if (num % 100 == 0)
-        return helper(num / 100) + "Hundred ";
+        if (num < 100)
+            return tens[num / 10] +
+            (num % 10 == 0 ? "" : " " + helper(num % 10));
 
-    return helper(num / 100) + "Hundred " + helper(num % 100);
-}
-        if (num < 1000000) {
-    if (num % 1000 == 0)
-        return helper(num / 1000) + "Thousand ";
+        if (num < 1000)
+            return helper(num / 100) + " Hundred" +
+           (num % 100 == 0 ? "" : " " + helper(num % 100));
 
-    return helper(num / 1000) + "Thousand " + helper(num % 1000);
-}
-        if (num < 1000000000) {
-    if (num % 1000000 == 0)
-        return helper(num / 1000000) + "Million ";
+        if (num < 1000000)
+            return helper(num / 1000) + " Thousand" +
+           (num % 1000 == 0 ? "" : " " + helper(num % 1000));
 
-    return helper(num / 1000000) + "Million " + helper(num % 1000000);
-}
-        return helper(num/1000000000) + "Billion " + helper(num%1000000000);
+        if (num < 1000000000)
+            return helper(num / 1000000) + " Million" +
+           (num % 1000000 == 0 ? "" : " " + helper(num % 1000000));
+
+        return helper(num / 1000000000) + " Billion" +
+       (num % 1000000000 == 0 ? "" : " " + helper(num % 1000000000));
     }
     public String numberToWords(int num) {
         if(num == 0) return "Zero";
