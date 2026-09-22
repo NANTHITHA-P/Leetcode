746d76428706d1d38f87a0259e1dcf29ac1127ed
Nanthitha P <nanthithaponnusamy@gmail.com>
2026-09-22 06:10:41 +0530

    Time: 0 ms (100%), Space: 42.1 MB (57.68%) - LeetHub

---
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int closestX = 0,closestY = 0;
+        if(xCenter < x1){
+            closestX  = x1;
+        }
+        else if(xCenter > x2){
+            closestX  = x2;
+        }
+        else{
+            closestX = xCenter;
+        }
+        if(yCenter < y1){
+            closestY  = y1;
+        }
+        else if(yCenter > y2){
+            closestY  = y2;
+        }
+        else{
+            closestY = yCenter;
+        }
+        int dx = xCenter - closestX;
+        int dy = yCenter - closestY;
+         return dx * dx + dy *dy <= radius*radius;
+    }
+}
